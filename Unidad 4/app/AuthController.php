3869b10b02a0a6878f9716d19e0e6ef16c45190d
JoseLuisGL <jgomez_21@alu.uabcs.mx>
2024-10-15 11:28:15 -0700

    Actividad 13

---
+++ Unidad 4/app/AuthController.php
@@ -0,0 +1,51 @@
+<?php
+session_start();
+
+if(isset($_POST["action"])) {
+    switch($_POST["action"]) {
+        case "access":
+            $authController = new AuthController();
+
+            $email = $_POST["email"];
+            $password = $_POST["password"];
+
+            $authController->login($email, $password);
+            break;
+    }
+}
+
+class AuthController{
+
+    public function login($email=null, $password=null){
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+          CURLOPT_URL => 'https://crud.jonathansoto.mx/api/login',
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_ENCODING => '',
+          CURLOPT_MAXREDIRS => 10,
+          CURLOPT_TIMEOUT => 0,
+          CURLOPT_FOLLOWLOCATION => true,
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_CUSTOMREQUEST => 'POST',
+          CURLOPT_POSTFIELDS => array('email' => $email,'password' => $password),
+          CURLOPT_HTTPHEADER => array(
+            'Cookie: XSRF-TOKEN=eyJpdiI6IkkxcC9wNU1JSkpBUnl5N3Q3VlUrNHc9PSIsInZhbHVlIjoiTjhWUjh3WEFIWkE2RVJEUW1sR0orTkRlREZEd1dGaTBMYlFuUkVSeTNUTVBKT0N2OFdPOFlvS210ZHpWalFZRDlONWQzUEZJMEtFZGxTSGdmU1cweXFpbU5MVTcwTkxucjBVZmxsWnpFUkFuT2taVUZUY3RUbFI4VGdXUFpuVEkiLCJtYWMiOiI0MzA3ZDYwZGZjN2QzZWM2ZDNmY2VlZDhjMjgwOWM2OGExNzM3YzY2Nzk2YjM2NDVjM2NjNDIwZGJhNzg5ZTZiIiwidGFnIjoiIn0%3D; apicrud_session=eyJpdiI6IjJGcmZWaWZtOG9SSXdoRTVDV3RrUGc9PSIsInZhbHVlIjoibjVqVlVKSisxRngwR0p1cjI3MmFNZndlZjFUaFdHWnVQTzdqMGYvZHJ5RVF6MkNTdVdFSTFVKzQ2K0x1MTlnTjZDc3lMeGFLbFhwQXJMei9Tbm5ENkJHbEhBRjVIZDExVXlQSzZsSzhIMWYyT3loUFAxbDhCWWoraUZobVBYUGQiLCJtYWMiOiIzNWFmOWMwMWVmNTY5MDMyOTYxMzhkZWFiNTdiZDAyZWEzMDFhMjI4MGZiYjY1NjdiNTQ3NGFiN2M1OTkzZThkIiwidGFnIjoiIn0%3D'
+          ),
+        ));
+        
+        $response = curl_exec($curl);
+        
+        curl_close($curl);
+        echo $response;
+
+        
+    }
+
+
+}
+
+
+
+?>
---
+++ Unidad 4/login.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Login</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col-1"></div>
+      <div class="col-10">
+        <div class="row">
+          <div class="col">
+            <img src="login.jpg" class="card-img-top" alt="...">
+          </div>
+          <div class="col">
+            <img src="logoZa.jpg" class="img-thumbnail" alt="...">
+            <form action="app/AuthController.php" method="POST">
+              <h2>Sign into your account</h2>
+              <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required name="email">
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+              </div>
+              <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" required name="password">
+              </div>
+              <div class="mb-3 form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+              </div>
+              <input type="hidden" name="action" value="access" />
+              <div class="d-grid gap-2 col-6 mx-auto">
+                <button class="btn btn-primary" type="submit">Login</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
